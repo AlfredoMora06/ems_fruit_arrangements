73c3d8e38dd0d3ba3e19fdfe47e34065959997c9
Alfredo Morales <alfredomorales@Alfredos-MacBook-Pro.local>
2024-02-06 17:57:49 -0500

    fixes made

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
+import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import AppBar from "@mui/material/AppBar"
 import Box from "@mui/material/Box"
 import Toolbar from "@mui/material/Toolbar"
 import Container from "@mui/material/Container"
 import Button from "@mui/material/Button"
-import { Fab, Grid } from "@mui/material"
+import { Fab, Grid, List, ListItem, ListItemButton, ListItemText, SwipeableDrawer } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import IconButton from "@mui/material/IconButton"
 
 //@ts-ignore
 import Logo from "../assets/Logo.svg"
@@ -13,20 +16,83 @@ import Logo from "../assets/Logo.svg"
 export default function Navbar(): JSX.Element {
   const location = useLocation()  
   const navigate = useNavigate()
+  const [drawer, setDrawer] = React.useState<boolean>(false)
+
+  const toggleDrawer = (open: boolean) =>  (event: React.KeyboardEvent | React.MouseEvent) => { 
+    setDrawer(open)
+  }
+
+  const handleCloseNavMenuRedirect = (link: string) => {
+    navigate(link)
+    toggleDrawer(false)
+  }
 
   const pages = [
     { title: "Home", link: "../" },
     { title: "About", link: "../0/about" },
+    { title: "Reviews", link: "../0/reviews" },
     { title: "Contact Us", link: "../0/contact" },
   ]
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {pages.map((page) => {
+          const samePathname = location.pathname === page.link.slice(2)
+
+          return (
+            <ListItem key={page.title} disablePadding sx={{width: '100%'}}>
+              <ListItemButton onClick={() => {handleCloseNavMenuRedirect(page.link)}}>
+                <ListItemText primary={page.title} sx={{fontWeight: 500, color: samePathname ? 'white' : '#D89D44'}}/>
+              </ListItemButton>
+            </ListItem>
+          )
+        })}
+      </List>
+    </Box>
+  )
 
   return (
     <AppBar
       position="static"
-      sx={{ background: "#826652", boxShadow: "none", padding: 4, display: { xs: "none", md: "flex" } }}
+      sx={{ background: "#826652", boxShadow: "none", paddingY: 1, paddingX: 4 }}
     >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <Grid container justifyContent={'space-between'}>
+              <Grid item xs={1} mt={1}>
+                {/* <img width={"100%"} src={ArtLogo} alt="Logo" /> */}
+              </Grid>
+              <IconButton
+                size='large'
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={toggleDrawer(true)}
+                sx={{color: '#D89D44'}}
+              >
+                <MenuIcon />
+              </IconButton>
+              <SwipeableDrawer
+                anchor="right"
+                PaperProps={{
+                  sx: { width: "50%" , overflow: "hidden", backgroundColor: '#826652'},
+                }}
+                open={drawer}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+              >
+                {list()}
+              </SwipeableDrawer>
+            </Grid>
+          </Box>
+
           <Box sx={{ flexGrow: 2, display: { xs: "none", md: "flex" } }}>
             <Grid item xs={1} >
               <img width={"70%"} src={Logo} alt="Logo" style={{borderRadius: 50}} />
@@ -55,7 +121,7 @@ export default function Navbar(): JSX.Element {
                 )
               })}
             </Grid>
-            <Grid container item xs={1.5} alignContent={"center"}>
+            <Grid container item xs={2} alignContent={"center"} justifyContent={"flex-end"}>
               <Fab
                 variant="extended"
                 style={{
